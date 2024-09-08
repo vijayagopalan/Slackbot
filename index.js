ae82587651cae53046d6f56d744c211fcafac6f9
vijay <vijayagopalan190598@gmail.com>
2024-09-08 17:04:00 +0530

    code update

--- index.js
+++ index.js
@@ -1,160 +1,65 @@
 const { App } = require('@slack/bolt');
 require('dotenv').config();
 let eventUser;
+const failure = {
+    apiFailureKeyWords: ['api', 'failure'],
+    readmemberApiFailureKeyWords: ['api', 'failure', 'readmember'],
+    widgetFailure: ['widget', 'not', 'loading']
+}
+
+const priority = {
+    apiFailureKeyWords: 1,
+    readmemberApiFailureKeyWords: 2,
+    widgetFailure: 3
+}
 const app = new App({
-    token: null,
-    signingSecret: null
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
 (async () => {
     await app.start(process.env.PORT || 3000)
-    console.log('Hi')
 })()
 
+const blockBuilders = (cases) => {
+    const details = cases?.filter((item) => item !== undefined);
+    if (details.length > 0) {
+        const blocks = [];
+        return blocks;
+    }
+    return [
+        {
+            "type": "section",
+            "text": {
+                "type": "plain_text",
+                "text": `we cant identify you request can you please give some more details`,
+                "emoji": true
+            }
+        }
+    ]
 
-app.event('app_home_opened', ({ event, say, client }) => {
-    eventUser = event.user;
-    console.log(event)
-    try {
-        const result = client.views.publish({
+}
+app.event('message', ({ event, say, client }) => {
+    if (event.channel === 'C07JM0KSM2T') {
+        const userText = event.text;
+        say(`hi <@${event.user}>, thanks for reaching War room chat`)
+        const keys = Object.keys(failure);
+        const failureCase = keys.map((scenario) => {
+            if (failure[scenario].every(substring => userText.includes(substring))) {
+                return scenario;
+            }
+        });
+        const blocks = blockBuilders(failureCase);
+        client.conversations.create();
+        client.views.publish({
             user_id: event.user,
             view: {
                 type: 'home',
                 callback_id: 'home_view',
-                "blocks": [
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "plain_text",
-                            "text": `hi ${event.user} how can i help you?`,
-                            "emoji": true
-                        }
-                    },
-                    {
-                        "type": "actions",
-                        "elements": [
-                            {
-                                "type": "radio_buttons",
-                                "options": [
-                                    {
-                                        "text": {
-                                            "type": "plain_text",
-                                            "text": "Api Failure",
-                                            "emoji": true
-                                        },
-                                        "value": "value-0"
-                                    },
-                                    {
-                                        "text": {
-                                            "type": "plain_text",
-                                            "text": "Widget Failure",
-                                            "emoji": true
-                                        },
-                                        "value": "value-1"
-                                    },
-                                    {
-                                        "text": {
-                                            "type": "plain_text",
-                                            "text": "Access Needed",
-                                            "emoji": true
-                                        },
-                                        "value": "value-2"
-                                    }
-                                ],
-                                "action_id": "actionId-0"
-                            }
-                        ]
-                    }
-                ]
+                blocks: blocks
             }
         })
     }
-    catch (e) {
-        console.log(e)
-    }
-});
+})
 
-app.action('actionId-0', ({ action, client }) => {
-    console.log("hello", action.selected_option.text.text)
-    try {
-        const result = client.views.publish({
-            user_id: eventUser,
-            view: {
-                type: 'home',
-                callback_id: 'home_view',
-                "blocks": [
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Selected option : " + action.selected_option.text.text,
-                            "emoji": true
-                        }
-                    },
-                    {
-                        "type": "actions",
-                        "elements": [
-                            {
-                                "type": "radio_buttons",
-                                "options": [
-                                    {
-                                        "text": {
-                                            "type": "plain_text",
-                                            "text": "Update Servicing Experience Failure",
-                                            "emoji": true
-                                        },
-                                        "value": "value-0"
-                                    },
-                                    {
-                                        "text": {
-                                            "type": "plain_text",
-                                            "text": "Read Servicing Experience Failure",
-                                            "emoji": true
-                                        },
-                                        "value": "value-1"
-                                    },
-                                    {
-                                        "text": {
-                                            "type": "plain_text",
-                                            "text": "Read Member Failure",
-                                            "emoji": true
-                                        },
-                                        "value": "value-2"
-                                    }
-                                ],
-                                "action_id": "api_Failure"
-                            }
-                        ]
-                    }
-                ]
-            }
-        })
-    }
-    catch (e) {
-        console.log(e)
-    }
-})
-app.action('api_Failure', ({ action, client }) => {
-    console.log("hello", action.selected_option.text.text)
-    try {
-        const result = client.views.publish({
-            user_id: eventUser,
-            view: {
-                type: 'home',
-                callback_id: 'home_view',
-                "blocks": [
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Raising ticket and Assigning to team",
-                            "emoji": true
-                        }
-                    }]
-            }
-        })
-    }
-    catch (e) {
-        console.log(e)
-    }
-})
+
---
+++ refference.js
@@ -0,0 +1,146 @@
+app.event('app_home_opened', ({ event, say, client }) => {
+    eventUser = event.user;
+    console.log(event)
+    try {
+        const result = client.views.publish({
+            user_id: event.user,
+            view: {
+                type: 'home',
+                callback_id: 'home_view',
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "plain_text",
+                            "text": `hi ${event.user} how can i help you?`,
+                            "emoji": true
+                        }
+                    },
+                    {
+                        "type": "actions",
+                        "elements": [
+                            {
+                                "type": "radio_buttons",
+                                "options": [
+                                    {
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "Api Failure",
+                                            "emoji": true
+                                        },
+                                        "value": "value-0"
+                                    },
+                                    {
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "Widget Failure",
+                                            "emoji": true
+                                        },
+                                        "value": "value-1"
+                                    },
+                                    {
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "Access Needed",
+                                            "emoji": true
+                                        },
+                                        "value": "value-2"
+                                    }
+                                ],
+                                "action_id": "actionId-0"
+                            }
+                        ]
+                    }
+                ]
+            }
+        })
+    }
+    catch (e) {
+        console.log(e)
+    }
+});
+
+app.action('actionId-0', ({ action, client }) => {
+    console.log("hello", action.selected_option.text.text)
+    try {
+        const result = client.views.publish({
+            user_id: eventUser,
+            view: {
+                type: 'home',
+                callback_id: 'home_view',
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Selected option : " + action.selected_option.text.text,
+                            "emoji": true
+                        }
+                    },
+                    {
+                        "type": "actions",
+                        "elements": [
+                            {
+                                "type": "radio_buttons",
+                                "options": [
+                                    {
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "Update Servicing Experience Failure",
+                                            "emoji": true
+                                        },
+                                        "value": "value-0"
+                                    },
+                                    {
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "Read Servicing Experience Failure",
+                                            "emoji": true
+                                        },
+                                        "value": "value-1"
+                                    },
+                                    {
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "Read Member Failure",
+                                            "emoji": true
+                                        },
+                                        "value": "value-2"
+                                    }
+                                ],
+                                "action_id": "api_Failure"
+                            }
+                        ]
+                    }
+                ]
+            }
+        })
+    }
+    catch (e) {
+        console.log(e)
+    }
+})
+app.action('api_Failure', ({ action, client }) => {
+    console.log("hello", action.selected_option.text.text)
+    try {
+        const result = client.views.publish({
+            user_id: eventUser,
+            view: {
+                type: 'home',
+                callback_id: 'home_view',
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Raising ticket and Assigning to team",
+                            "emoji": true
+                        }
+                    }]
+            }
+        })
+    }
+    catch (e) {
+        console.log(e)
+    }
+})
